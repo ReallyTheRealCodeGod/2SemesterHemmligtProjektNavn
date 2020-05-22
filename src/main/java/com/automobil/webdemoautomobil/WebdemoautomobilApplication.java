5eac67f4b5993c1fcd0e3b5e6e997d85500c8796
Peter Bandsholm <lil.jdean@gmail.com>
2020-05-22 11:27:41 +0200

    commit

---
+++ src/main/java/com/automobil/webdemoautomobil/WebdemoautomobilApplication.java
@@ -0,0 +1,13 @@
+package com.automobil.webdemoautomobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdemoautomobilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdemoautomobilApplication.class, args);
+    }
+
+}
