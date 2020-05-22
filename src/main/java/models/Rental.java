5f6c8f52d380c8c4e6b60b694bcc7a56bc73e46d
Peter Bandsholm <lil.jdean@gmail.com>
2020-05-22 14:36:25 +0200

    commit

---
+++ src/main/java/models/Rental.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Rental {
+
+    private int id;
+    private int accumulatedPrice;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String pickUpLoc;
+    private String dropOffLoc;
+
+
+}
---
+++ src/test/java/junittest/JdbcTest.java
@@ -0,0 +1,15 @@
+package junittest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcTest {
+    @Test
+    public void initializationTest(){
+
+
+
+    }
+
+}
