d4e869f859d3200bfb77b4832c6ae8c9e5217a4a
Rasmussejer <rasm897c@stud.kea.dk>
2020-07-10 10:09:21 +0200

    SQL tabeller updateret

--- src/main/resources/sql/mysql_tabeller.sql
+++ src/main/resources/sql/mysql_tabeller.sql
@@ -30,12 +30,12 @@ INSERT INTO `autocamper_db`.`autocamper_type`
  `description`)
 VALUES
 ("Farrari","Moccasin","1332","2010","4287","49","26","104","3","12","4","14","4","Dinah my dear! Let this be a book of rules for shutting people up like telescopes: this time it all seemed quite dull and stupid for life to go among mad people,' Alice remarked. 'Oh, you foolish."),
-("Skiles IV","PaleGoldenRod","878","1983","2126","30","50","108","5","8","7","13","4","NOT a serpent!' said Alice angrily. 'It wasn't very civil of you to death.""' 'You are old,' said the Cat. '--so long as it was as much as she could not think of any good reason, and as for the White."),
-("Jacobson II","AntiqueWhite","4527","2018","1140","50","38","168","5","7","4","11","4","For instance, suppose it doesn't matter much,' thought Alice, 'to speak to this last word with such sudden violence that Alice could hardly hear the very tones of the fact. 'I keep them to be a."),
+("Toyota","S55 AMG","878","1983","2126","30","50","108","5","8","7","13","4","NOT a serpent!' said Alice angrily. 'It wasn't very civil of you to death.""' 'You are old,' said the Cat. '--so long as it was as much as she could not think of any good reason, and as for the White."),
+("HONDA","TRX450ER","4527","2018","1140","50","38","168","5","7","4","11","4","For instance, suppose it doesn't matter much,' thought Alice, 'to speak to this last word with such sudden violence that Alice could hardly hear the very tones of the fact. 'I keep them to be a."),
 ("Volkswagen","Gainsboro","2757","2020","4447","53","27","136","3","9","7","13","4","I had it written down: but I can't be civil, you'd better finish the story for yourself.' 'No, please go on!' Alice said very humbly	 'I won't indeed!' said the King, 'unless it was a different."),
-("Runte III","LightSalmon","2497","1971","3552","50","34","182","6","10","6","10","4","She said the Duchess, who seemed to be sure! However, everything is to-day! And yesterday things went on to himself as he spoke, 'we were trying--' 'I see!' said the March Hare said to itself in a."),
+("HEYLEY DAVIDSON","ROAD KING","2497","1971","3552","50","34","182","6","10","6","10","4","She said the Duchess, who seemed to be sure! However, everything is to-day! And yesterday things went on to himself as he spoke, 'we were trying--' 'I see!' said the March Hare said to itself in a."),
 ("Toyota","Yellow","4546","1990","4857","65","42","157","3","8","5","14","4","Who would not give all else for two reasons. First, because I'm on the back. At last the Dodo solemnly presented the thimble, saying 'We beg your pardon!' said the King: 'leave out that one of the."),
-("modelVerla Toy","Aquamarine","4171","2009","3497","40","40","121","3","8","6","15","4","Northumbria, declared for him: and even Stigand, the patriotic archbishop of Canterbury, found it made no mark	 but he now hastily began again, using the ink, that was said, and went on in these."),
+("Farrari","GRAND CARAVAN","4171","2009","3497","40","40","121","3","8","6","15","4","Northumbria, declared for him: and even Stigand, the patriotic archbishop of Canterbury, found it made no mark	 but he now hastily began again, using the ink, that was said, and went on in these."),
 ("Zusuki","MidnightBlue","1594","2006","4009","34","52","100","4","9","4","13","4","Caterpillar's making such a curious croquet-ground in her hands, and began:-- 'You are old,' said the Duchess	 'and that's why. Pig!' She said the Cat	 and this was of very little use without my.");
 
 
@@ -44,11 +44,11 @@ INSERT INTO autocamper_db.autocamper
 (DEFAULT, '1000', 2, NULL, 'Zusuki', 'MidnightBlue'),
 (DEFAULT, '2000', 1, NULL, 'Toyota', 'Yellow'),
 (DEFAULT, '1034', 3, NULL, 'Volkswagen', 'Gainsboro'),
-(DEFAULT, '3000', 2, NULL, 'Farrari', 'Moccasin');
+(DEFAULT, '3000', 4, NULL, 'Farrari', 'Moccasin'),
 (DEFAULT, '2000', 1, NULL, 'Toyota', 'S55 AMG'),
-(DEFAULT, '1034', 3, NULL, 'HONDA', 'TRX450ER'),
-(DEFAULT, '3000', 2, NULL, 'HARLEY DAVIDSON', 'ROAD KING');
-(DEFAULT, '3000', 2, NULL, 'Farrari', 'GRAND CARAVAN');
+(DEFAULT, '1034', 4, NULL, 'HONDA', 'TRX450ER'),
+(DEFAULT, '3000', 5, NULL, 'HEYLEY DAVIDSON', 'ROAD KING'),
+(DEFAULT, '3000', 5, NULL, 'Farrari', 'GRAND CARAVAN');
 
 
 INSERT INTO `autocamper_db`.`customer`
@@ -90,8 +90,6 @@ INSERT INTO `autocamper_db`.`maintenance`
  `fk_autocamper_id`)
 VALUES
 (DEFAULT, 50, 378, 200, "no notes", NULL,1, 1, 1, 1, 1, 1, 1, 1, 1, "2020-12-08", 1);
-
-INSERT INTO season(season_name,  charge_percentage, start_date, end_date) values ("high", 60, "2020-12-08", "2021-04-02");
 
 
 INSERT INTO `autocamper_db`.`rental`
